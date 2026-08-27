529c4819e1615f3a2be18656163b6b32d1410835
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2026-08-28 03:31:21 +0700

    fix: install.sh compatible with curl installation

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Installing ros2gen CLI..."
-sudo cp bin/ros2gen /usr/local/bin/ros2gen
+
+sudo curl -fsSL https://raw.githubusercontent.com/ahmdmarzuki/ros2-pkg-generator/main/bin/ros2gen -o /usr/local/bin/ros2gen
+
 sudo chmod +x /usr/local/bin/ros2gen
 
 echo "✅ Installed! You can now run 'ros2gen <robot_name>' from anywhere."
